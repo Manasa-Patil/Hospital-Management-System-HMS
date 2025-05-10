539833cbc8f1bbdbec96da3ea8e3869113ef31ac
tejaswinivb11 <teju.vb11@gamil.com>
2025-05-10 22:01:21 +0530

    Remove package-lock.json from the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Welcome from "./Welcome";
 import DoctorRoutes from "./routes/DoctorRoutes";
@@ -10,27 +10,25 @@ import "./Login.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Login />} />
-          <Route path="/welcome" element={<Welcome />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/welcome" element={<Welcome />} />
 
-          {/* Doctor Routes */}
-          <Route path="/doctor/*" element={<DoctorRoutes />} />
+        {/* Doctor Routes */}
+        <Route path="/doctor/*" element={<DoctorRoutes />} />
 
-          {/* Admin Routes */}
-          <Route path="/admin/*" element={<AdminRoutes />} />
+        {/* Admin Routes */}
+        <Route path="/admin/*" element={<AdminRoutes />} />
 
-          {/* Patient Routes */}
-          <Route path="/patient/*" element={<PatientRoutes />} />
+        {/* Patient Routes */}
+        <Route path="/patient/*" element={<PatientRoutes />} />
 
-          {/* Catch-all route */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+        {/* 404 - Page Not Found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
